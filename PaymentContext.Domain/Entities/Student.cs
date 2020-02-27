29664631067cd3eab5c5328feb15588d82fd07fd
Francisco Rodrigues <francisco.sousa@cg.df.gov.br>
2020-02-27 14:39:43 -0300

    Corrupção no código

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -1,14 +1,41 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PaymentContext.Domain.Entities
 {
     public class Student
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Document { get; set; }
-        public string Email { get; set; }
-        public List<Subscription> Subscription { get; set; }
-        public string Adress { get; set; }
+        private IList<Subscription> _subscriptions;
+        public Student(string firstName, string lastName, string document, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Document = document;
+            Email = email;
+            _subscriptions = new List<Subscription>();
+
+            if(firstName.Length == 0)
+            {
+                throw new Exception("Nome inválido");
+            }
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Document { get; private set; }
+        public string Email { get; private set; }
+        public string Address { get; private set; }
+        public IReadOnlyCollection<Subscription> Subscriptions { get{ return _subscriptions.ToArray(); } }
+
+        public void AddSubscription(Subscription subscription)
+        {
+            foreach(var sub in Subscriptions)
+            {
+                sub.Deactivate();
+            }
+
+            _subscriptions.Add(subscription);
+        }
     }
 }
---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Francisco", "Rodrigues", "123", "f@g.com");
+            student.AddSubscription(subscription);
+        }
+    }
+}
