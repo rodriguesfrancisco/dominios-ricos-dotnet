a6cd7fdb4db5e9fac0443223eb7f7a7cba18b13c
Francisco Victor Rodrigues de Sousa <francisco.sousa@corregedoria.df>
2020-03-09 15:13:24 -0300

    Commands

---
+++ PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
@@ -0,0 +1,32 @@
+using System;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Domain.Commands
+{
+    public class CreateBoletoSubscriptionCommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public string BarCode { get; set; }
+        public string BoletoNumber { get; set; }
+        public string PaymentNumber { get; set; }
+        public DateTime PaidDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public decimal Total { get; set; }
+        public decimal TotalPaid { get; set; }
+        public string Payer { get; set; }
+        public string PayerDocument { get; set; }
+        public EDocumentType PayerDocumentType { get; set; }
+        public string PayerEmail { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string Neighbourhood { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
