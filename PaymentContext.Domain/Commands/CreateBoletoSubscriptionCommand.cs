f22b9dd7b9787773fc089c3ec2ae2dd1a4d57521
Francisco Victor Rodrigues de Sousa <francisco.sousa@corregedoria.df>
2020-03-10 09:53:03 -0300

    Fail Fast Validations

--- PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
+++ PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
@@ -1,10 +1,13 @@
 using System;
+using Flunt.Notifications;
+using Flunt.Validations;
 using PaymentContext.Domain.Enums;
 using PaymentContext.Domain.ValueObjects;
+using PaymentContext.Shared.Commands;
 
 namespace PaymentContext.Domain.Commands
 {
-    public class CreateBoletoSubscriptionCommand
+    public class CreateBoletoSubscriptionCommand : Notifiable, ICommand
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -28,5 +31,15 @@ namespace PaymentContext.Domain.Commands
         public string State { get; set; }
         public string Country { get; set; }
         public string ZipCode { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasMinLen(FirstName, 3, "Name.FirstName", "Nome deve conter pelo menos 3 caracteres")
+                .HasMinLen(LastName, 3, "Name.LastName", "Sobrenome deve conter pelo menos 3 caracteres")
+                .HasMaxLen(FirstName, 4, "Name.LastName", "Nome deve conter no máximo 40 caracteres")
+            );
+        }
     }
 }
