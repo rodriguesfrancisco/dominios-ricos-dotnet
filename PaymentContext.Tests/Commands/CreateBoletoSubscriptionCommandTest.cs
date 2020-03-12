246fc86610bd6742ed586139d7428be7751ecb96
Francisco Victor Rodrigues de Sousa <francisco.sousa@corregedoria.df>
2020-03-12 14:51:37 -0300

    Testando os Commands

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTest.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Commands;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTest
+    {
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenNameIsInvalid()
+        {
+            var command = new CreateBoletoSubscriptionCommand();
+            command.FirstName = "";
+
+            command.Validate();
+            Assert.AreEqual(false, command.Valid);
+        }
+    }
+}
