86784fff02e1749a7d7f362d6a41ee916367e5b8
Francisco Victor Rodrigues de Sousa <francisco.sousa@corregedoria.df>
2020-03-12 17:36:34 -0300

    Testando Queries

--- PaymentContext.Domain/Queries/StudentQueries.cs
+++ PaymentContext.Domain/Queries/StudentQueries.cs
@@ -6,7 +6,7 @@ namespace PaymentContext.Domain.Queries
 {
     public static class StudentQueries
     {
-        public static Expression<Func<Student, bool>> GetStudent(string document)
+        public static Expression<Func<Student, bool>> GetStudentInfo(string document)
         {
             return x => x.Document.Number == document;
         }
